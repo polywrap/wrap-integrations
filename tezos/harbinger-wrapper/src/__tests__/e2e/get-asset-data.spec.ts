538fd07099cd091394a3ad22d96440042ec0c21b
Abdul-Harisu Inusah <abdul@blockwatch.cc>
2022-01-23 22:30:55 +0000

    handle oracle contract adresses internally for active networks

--- tezos/harbinger-wrapper/src/__tests__/e2e/get-asset-data.spec.ts
+++ tezos/harbinger-wrapper/src/__tests__/e2e/get-asset-data.spec.ts
@@ -25,27 +25,23 @@ describe("getAssetData", () => {
         await stopTestEnvironment()
     })
 
-    it("should get asset data for `XTZ-USD`", async () => {
+    it("should get asset data for `XTZ-USD` on mainnet", async () => {
         const response =  await client.query<{ getAssetData: QuerySchema.GetAssetResponse}>({
           uri: ensUri,
           query: `
             query {
               getAssetData(
-                oracleContractAddress: $oracleContractAddress,
-                assetCode: $assetCode
-                connection: $connection
+                assetCode: $assetCode,
+                network: MAINNET
               )
             }
           `,
           variables: {
-            oracleContractAddress: "KT1Jr5t9UvGiqkvvsuUbPJHaYx24NzdUwNW9",
             assetCode: "XTZ-USD",
-            connection: {
-              networkNameOrChainId: "mainnet"
-            }
+            network: "granadanet",
           }
         })
-        
+
         expect(response.errors).toBeUndefined()
         expect(response.data).toBeDefined()
         expect(response.data?.getAssetData).toBeDefined()
@@ -57,5 +53,97 @@ describe("getAssetData", () => {
         expect(response.data?.getAssetData.volume).toBeDefined()
         expect(response.data?.getAssetData.endPeriod).toBeDefined()
         expect(response.data?.getAssetData.startPeriod).toBeDefined()
+    })
+
+    it("should get asset data for `XTZ-USD` on granadanet", async () => {
+      const response =  await client.query<{ getAssetData: QuerySchema.GetAssetResponse}>({
+        uri: ensUri,
+        query: `
+        query {
+          getAssetData(
+            assetCode: $assetCode,
+            network: GRANADANET
+            )
+          }
+          `,
+          variables: {
+            network: "granadanet",
+            assetCode: "XTZ-USD",
+            oracleContractAddress: "KT1Jr5t9UvGiqkvvsuUbPJHaYx24NzdUwNW9",
+            connection: {
+              networkNameOrChainId: "mainnet"
+            }
+          }
+        })
+      
+      expect(response.errors).toBeUndefined()
+      expect(response.data).toBeDefined()
+      expect(response.data?.getAssetData).toBeDefined()
+      expect(response.data?.getAssetData.low).toBeDefined()
+      expect(response.data?.getAssetData.open).toBeDefined()
+      expect(response.data?.getAssetData.high).toBeDefined()
+      expect(response.data?.getAssetData.asset).toBeDefined()
+      expect(response.data?.getAssetData.close).toBeDefined()
+      expect(response.data?.getAssetData.volume).toBeDefined()
+      expect(response.data?.getAssetData.endPeriod).toBeDefined()
+      expect(response.data?.getAssetData.startPeriod).toBeDefined()
+    })
+
+    it("should get asset data for `XTZ-USD` on custom network", async () => {
+      const response =  await client.query<{ getAssetData: QuerySchema.GetAssetResponse}>({
+        uri: ensUri,
+        query: `
+        query {
+            getAssetData(
+              assetCode: $assetCode,
+              network: CUSTOM,
+              custom: $custom
+            )
+          }
+          `,
+          variables: {
+            custom: {
+              oracleContractAddress: "KT1Jr5t9UvGiqkvvsuUbPJHaYx24NzdUwNW9",
+              connection: {
+                provider: "https://rpc.tzstats.com",
+                networkNameOrChainId: "mainnet"
+              },
+            },
+            assetCode: "XTZ-USD",
+          }
+        })
+      
+      expect(response.errors).toBeUndefined()
+      expect(response.data).toBeDefined()
+      expect(response.data?.getAssetData).toBeDefined()
+      expect(response.data?.getAssetData.low).toBeDefined()
+      expect(response.data?.getAssetData.open).toBeDefined()
+      expect(response.data?.getAssetData.high).toBeDefined()
+      expect(response.data?.getAssetData.asset).toBeDefined()
+      expect(response.data?.getAssetData.close).toBeDefined()
+      expect(response.data?.getAssetData.volume).toBeDefined()
+      expect(response.data?.getAssetData.endPeriod).toBeDefined()
+      expect(response.data?.getAssetData.startPeriod).toBeDefined()
+    })
+
+    it("should fail if get connection and oracle address is not provided when using custom network", async () => {
+      const response =  await client.query<{ getAssetData: QuerySchema.GetAssetResponse}>({
+        uri: ensUri,
+        query: `
+        query {
+            getAssetData(
+              assetCode: $assetCode,
+              network: CUSTOM
+            )
+          }
+          `,
+          variables: {
+            
+            assetCode: "XTZ-USD",
+          }
+        })
+
+      expect(response.errors).toBeDefined()
+      expect(response.data?.getAssetData).toBeUndefined()
     })
 })
--- tezos/harbinger-wrapper/src/query/index.ts
+++ tezos/harbinger-wrapper/src/query/index.ts
@@ -1,16 +1,45 @@
 import {
+  Network,
   Tezos_Query,
   GetAssetResponse,
+  Tezos_Connection,
   Input_getAssetData
 } from "./w3";
 import { getString, normalizeValue } from "../utils/common"
 
 import { JSON } from "assemblyscript-json"; 
 
+// Provide default oracle contract addresses for network
+// @link for reference - https://github.com/tacoinfra/harbinger
+// Below are contract addresses
+// NB: Florencenet is not supported because there are no available public rpc node
+// MainNet    -> KT1Jr5t9UvGiqkvvsuUbPJHaYx24NzdUwNW9
+// Granadanet -> KT1ENR6CK7cBWCtZt1G3PovwTw3FgSW472mS
 export function getAssetData(input: Input_getAssetData): GetAssetResponse {
+  if (input.network == Network.CUSTOM && input.custom === null) {
+    throw new Error(`custom network should have a valid connection and oracle contract address `)
+  }
+  let oracleContractAddress: string = "KT1Jr5t9UvGiqkvvsuUbPJHaYx24NzdUwNW9";
+  let connection: Tezos_Connection = {
+    provider: "https://rpc.tzstats.com",
+    networkNameOrChainId: "mainnet"
+  };
+  switch (input.network) {
+    case Network.GRANADANET:
+      connection = <Tezos_Connection> {
+        provider: "https://rpc.granada.tzstats.com",
+        networkNameOrChainId: "granadanet"  
+      }
+      oracleContractAddress = "KT1ENR6CK7cBWCtZt1G3PovwTw3FgSW472mS";
+      break;
+    case Network.CUSTOM:
+      connection = input.custom!.connection;
+      oracleContractAddress = input.custom!.oracleContractAddress;
+      break;
+  }
   const storageValue = Tezos_Query.getContractStorage({
-    address: input.oracleContractAddress,
-    connection: input.connection,
+    address: oracleContractAddress,
+    connection: connection,
     key: "oracleData",
     field: input.assetCode
   });
@@ -24,5 +53,5 @@ export function getAssetData(input: Input_getAssetData): GetAssetResponse {
       volume: normalizeValue(parseFloat(getString(assetData, "6"))),
       endPeriod: getString(assetData, "1"),
       startPeriod: getString(assetData, "0"),
-    };
+  };
 }
