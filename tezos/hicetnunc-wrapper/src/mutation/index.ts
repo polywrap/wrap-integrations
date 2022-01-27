2371d7200430ccdabc28f311e620652c3c47959e
Abdul-Harisu Inusah <abdul@blockwatch.cc>
2022-01-27 04:31:06 +0000

    initial template setup

---
+++ tezos/hicetnunc-wrapper/src/mutation/index.ts
@@ -0,0 +1,7 @@
+import {
+  Input_set,
+} from "./w3";
+
+export function set(input: Input_set): string {
+  return "set";
+}
---
+++ tezos/hicetnunc-wrapper/src/query/index.ts
@@ -0,0 +1,8 @@
+import {
+  Tezos_Query,
+  Input_get
+} from "./w3";
+
+export function get(input: Input_get): string {
+  return "get";
+}
