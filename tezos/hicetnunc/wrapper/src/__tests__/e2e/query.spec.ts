b853cd630c738e1aeb1f6d6b844817fc549baffe
Abdul-Harisu Inusah <abdul@blockwatch.cc>
2022-01-31 10:37:04 +0000

    hicetnunc wrapper initial commit

---
+++ tezos/hicetnunc/wrapper/src/__tests__/e2e/query.spec.ts
@@ -0,0 +1,120 @@
+import path from "path"
+import { Web3ApiClient } from "@web3api/client-js"
+import { buildAndDeployApi, initTestEnvironment, stopTestEnvironment } from "@web3api/test-env-js"
+
+import * as QuerySchema from "../../query/w3"
+import { getPlugins } from "../testUtils"
+
+jest.setTimeout(150000)
+
+describe("getBalanceOfData", () => {
+  let client: Web3ApiClient;
+  let ensUri: string;
+
+  beforeAll(async () => {
+    const { ensAddress, ethereum, ipfs } = await initTestEnvironment();
+    const apiPath = path.join(__dirname, "/../../../");
+    const api = await buildAndDeployApi(apiPath, ipfs, ensAddress);
+    ensUri = `ens/testnet/${api.ensDomain}`;
+    client = new Web3ApiClient({
+        plugins: getPlugins(ipfs, ensAddress, ethereum),
+    })
+  })
+
+  afterAll(async () => {
+    await stopTestEnvironment()
+  })
+
+  it("should return balance", async () => {
+    const response =  await client.query<{ getBalanceOf: QuerySchema.GetBalanceResponse}>({
+      uri: ensUri,
+      query: `
+        query {
+          getBalanceOf(
+            network: mainnet,
+            owner: $owner, 
+            token_id: $token_id 
+          )
+        }
+      `,
+      variables: {
+        owner: "tz1UBZUkXpKGhYsP5KtzDNqLLchwF4uHrGjw",
+        token_id: "152",
+      }
+    })
+
+    expect(response.errors).toBeUndefined()
+    expect(response.data).toBeDefined()
+    expect(response.data?.getBalanceOf).toBeDefined()
+    expect(response.data?.getBalanceOf.owner).toBeDefined()
+    expect(response.data?.getBalanceOf.token_id).toBeDefined()
+    expect(response.data?.getBalanceOf.balance).toBeDefined()
+  })
+
+  it("should get token metadata on mainnet", async () => {
+    const response =  await client.query<{ getTokenMetadata: QuerySchema.GetTokenMetadataResponse}>({
+      uri: ensUri,
+      query: `
+        query {
+          getTokenMetadata(
+            network: mainnet,
+            token_id: $token_id
+          )
+        }
+      `,
+      variables: {
+        token_id: "152",
+      }
+    })
+
+    expect(response.errors).toBeUndefined()
+    expect(response.data).toBeDefined()
+    expect(response.data?.getTokenMetadata.token_id).toBeDefined()
+    expect(response.data?.getTokenMetadata.ipfs_hash).toBeDefined()
+  })
+
+  it("should count token on mainnet", async () => {
+    const response =  await client.query<{ getTokenCountData: QuerySchema.GetTokenCountResponse}>({
+      uri: ensUri,
+      query: `
+        query {
+          getTokenCountData(
+            network: mainnet
+          )
+        }
+      `,
+      variables: {
+      }
+    })
+
+    expect(response.errors).toBeUndefined()
+    expect(response.data).toBeDefined()
+    expect(response.data?.getTokenCountData.tokenCount).toBeDefined()
+  })
+
+
+  it.only("should swap data", async () => {
+    const response =  await client.query<{ getSwapsData: QuerySchema.GetSwapsResponse}>({
+      uri: ensUri,
+      query: `
+        query {
+          getSwapsData(
+            network: mainnet,
+            swap_id: $swap_id
+          )
+        }
+      `,
+      variables: {
+        swap_id: "500004"
+      }
+    });
+    expect(response.errors).toBeUndefined()
+    expect(response.data).toBeDefined()
+    expect(response.data?.getSwapsData).toBeDefined()
+    expect(response.data?.getSwapsData.creator).toBeDefined()
+    expect(response.data?.getSwapsData.issuer).toBeDefined()
+    expect(response.data?.getSwapsData.objkt_amount).toBeDefined()
+    expect(response.data?.getSwapsData.objkt_id).toBeDefined()
+    expect(response.data?.getSwapsData.royalties).toBeDefined()
+  })
+})
---
+++ tezos/hicetnunc/wrapper/src/query/index.ts
@@ -0,0 +1,111 @@
+import {
+  Network,
+  Tezos_Query,
+  Tezos_Connection,
+  GetBalanceResponse,
+  Input_getBalanceOf,
+  GetTokenMetadataResponse,
+  GetTokenCountResponse,
+  GetSwapsResponse,
+  Input_getTokenMetadata,
+  Input_getTokenCountData,
+  Input_getSwapsData,
+  CustomConnection
+} from "./w3";
+import { getString } from "../utils/common"
+
+import { JSON } from "assemblyscript-json"; 
+
+class ConnectionDetails {
+  connection: Tezos_Connection;
+  contractAddress: string;
+}
+
+export function getBalanceOf(input: Input_getBalanceOf): GetBalanceResponse {
+  if (input.network == Network.custom && input.custom === null) {
+    throw new Error(`custom network should have a valid connection and contract address`);
+  }
+  const connectionDetails = getConnectionDetails(input.network, input.custom);
+  const balance = Tezos_Query.getContractStorage({
+    address: connectionDetails.contractAddress,
+    connection: connectionDetails.connection,
+    key: "ledger",
+    field: '["' + input.owner + '",' + input.token_id + ']',
+  });
+  return {
+      owner: input.owner,
+      token_id: input.token_id,
+      balance: balance
+  };
+} 
+
+export function getTokenMetadata(input: Input_getTokenMetadata): GetTokenMetadataResponse {
+  if (input.network == Network.custom && input.custom === null) {
+    throw new Error(`custom network should have a valid connection and contract address `)
+  }
+  const connectionDetails = getConnectionDetails(input.network, input.custom);
+  const storage = Tezos_Query.getContractStorage({
+    address: connectionDetails.contractAddress,
+    connection: connectionDetails.connection,
+    key: "token_metadata",
+    field: input.token_id
+  });
+  const metadata = <JSON.Obj>JSON.parse(storage);
+  const hash = <JSON.Obj>JSON.parse(getString(metadata, "token_info"));
+  return {
+      token_id: getString(metadata, "token_id"),
+      ipfs_hash: getString(hash, '""')
+  };
+}
+
+export function getTokenCountData(input: Input_getTokenCountData): GetTokenCountResponse {
+  if (input.network == Network.custom && input.custom === null) {
+    throw new Error(`custom network should have a valid connection and contract address`)
+  }
+  const connectionDetails = getConnectionDetails(input.network, input.custom);
+  const count = Tezos_Query.getContractStorage({
+    address: connectionDetails.contractAddress,
+    connection: connectionDetails.connection,
+    key: "all_tokens"
+  });
+  return {
+    tokenCount: count,
+  };
+}
+
+export function getSwapsData(input: Input_getSwapsData): GetSwapsResponse {
+  if (input.network == Network.custom && input.custom === null) {
+    throw new Error(`custom network should have a valid connection and oracle contract address `)
+  }
+  const connectionDetails = getConnectionDetails(input.network, input.custom);
+  const storage = Tezos_Query.getContractStorage({
+    address: "KT1HbQepzV1nVGg8QVznG7z4RcHseD5kwqBn",
+    connection: connectionDetails.connection,
+    key: "swaps",
+    field: input.swap_id
+  });
+  const swap = <JSON.Obj>JSON.parse(storage);
+  return {
+    creator: getString(swap, "creator"),
+    issuer: getString(swap, "issuer"),
+    objkt_amount: getString(swap, "objkt_amount"),
+    objkt_id: getString(swap, "objkt_id"),
+    royalties: getString(swap, "royalties"),
+  };
+}
+
+function getConnectionDetails(network: Network, custom: CustomConnection | null): ConnectionDetails {
+  let address: string = "KT1RJ6PbjHpwc3M5rw5s2Nbmefwbuwbdxton";
+  let connection: Tezos_Connection = {
+    provider: "https://rpc.tzstats.com",
+    networkNameOrChainId: "mainnet"
+  };
+  if (network == Network.custom) {
+    connection = custom!.connection;
+    address = custom!.contractAddress;
+  }
+  return {
+    connection: connection,
+    contractAddress: address,
+  }
+}
