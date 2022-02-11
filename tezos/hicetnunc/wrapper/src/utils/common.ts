815c6733b4a49f1e01bedcbfe696980da12a8efb
Abdul-Harisu Inusah <abdul@blockwatch.cc>
2022-02-11 19:13:22 +0000

    remove unused util

--- tezos/hicetnunc/wrapper/src/utils/common.ts
+++ tezos/hicetnunc/wrapper/src/utils/common.ts
@@ -8,10 +8,3 @@ export function getString(object: JSON.Obj, key: string): string {
     }
     return value
 }
-
-export function normalizeValue(input: f64): string {
-    const scale = Math.pow(10, 6)
-    const output = input
-    return output.toString()
-} 
-  
