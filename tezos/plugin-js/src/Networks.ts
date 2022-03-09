9bb6b7efa47553111b5d1d24272734d6352184bb
Abdul-Harisu Inusah <abdul@blockwatch.cc>
2022-03-09 21:59:26 +0000

    fix: add correct name

--- tezos/plugin-js/src/Networks.ts
+++ tezos/plugin-js/src/Networks.ts
@@ -1,7 +1,7 @@
 const TezosRpcNodes: { [key: string]: string } = {
   mainnet: "https://rpc.tzstats.com",
   granadanet: "https://rpc.granada.tzstats.com",
-  hangzhou: "https://rpc.hangzhou.tzstats.com",
+  hangzhounet: "https://rpc.hangzhou.tzstats.com",
 };
 
 export const getProvider: (name: string) => string = (name) =>
