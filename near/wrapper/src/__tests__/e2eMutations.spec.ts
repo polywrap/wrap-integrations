2d2a5761fe2700eea8f5babe4fb090bb5fd97d31
Mike <mikelrap@gmail.com>
2022-05-10 15:40:34 +0300

    createAccountTest

--- near/wrapper/src/__tests__/e2eMutations.spec.ts
+++ near/wrapper/src/__tests__/e2eMutations.spec.ts
@@ -2,11 +2,11 @@ import * as testUtils from "./testUtils";
 import { Near_FinalExecutionOutcome } from "../w3";
 
 import { Web3ApiClient } from "@web3api/client-js";
-import { NearPluginConfig } from "../../../plugin-js"; //TODO change to appropriate package
+import { KeyPair, NearPluginConfig } from "../../../plugin-js"; //TODO change to appropriate package
 import * as nearApi from "near-api-js";
 import { buildAndDeployApi, initTestEnvironment, stopTestEnvironment } from "@web3api/test-env-js";
 import path from "path";
-//import BN from "bn.js";
+import { BN } from "bn.js";
 
 jest.setTimeout(360000);
 
@@ -39,10 +39,19 @@ describe("e2e", () => {
   afterAll(async () => {
     await stopTestEnvironment();
   });
-  /*   it("Create account", async () => {
+
+  it("Create account", async () => {
     const newAccountId = testUtils.generateUniqueString("test");
-    //const newAccountPublicKey = testUtils.publicKeyFromStr("9AhWenZ3JddamBoyMqnTbp7yVbRuvqAv3zwfrWgfVRJE");
-    const newPublicKey = await near.connection.signer.createKey(newAccountId, near.connection.networkId);
+    const keyPair = KeyPair.fromRandom("ed25519");
+
+    const accountToCreate = await near.account(newAccountId);
+
+    expect(accountToCreate.state()).rejects.toThrow();
+
+    const { amount } = await workingAccount.state();
+    const newAmount = new BN(amount).div(new BN(10)).toString();
+
+    const newPublicKey = keyPair.getPublicKey();
 
     const result = await client.query<{ createAccount: Near_FinalExecutionOutcome }>({
       uri: apiUri,
@@ -57,14 +66,26 @@ describe("e2e", () => {
       variables: {
         newAccountId: newAccountId,
         publicKey: newPublicKey,
-        amount: "100000000000000000000",
+        amount: newAmount,
         signerId: testUtils.testAccountId,
       },
     });
 
     expect(result.errors).toBeFalsy();
     expect(result.data).toBeTruthy();
-  }); */
+
+    const creationResult = result.data!.createAccount;
+
+    expect(creationResult).toBeTruthy();
+    expect(creationResult.status.failure).toBeFalsy();
+    expect(creationResult.status.SuccessValue).toBeDefined();
+
+    const accountCreated = await near.account(newAccountId);
+
+    expect(accountCreated.state()).resolves;
+
+    await accountCreated.deleteAccount(testUtils.testAccountId);
+  });
 
   /* it("Add key", async () => {
     const keyPair = nearApi.utils.KeyPair.fromRandom("ed25519");
