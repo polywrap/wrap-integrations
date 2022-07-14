ee0d463c19c83710d0882abcc2bc7143bf4638de
Jovansonlee Cesar <ivanceras@gmail.com>
2022-07-14 17:15:06 +0800

    feat: update to polywrap 0.2.0, try using hardcoded ensAddress

--- protocol/substrate/core-wrapper/example_app/index.ts
+++ protocol/substrate/core-wrapper/example_app/index.ts
@@ -1,10 +1,19 @@
 import { Uri, PolywrapClient } from "@polywrap/client-js";
 import { Substrate_Module } from "./wrap";
 import { ipfsPlugin } from "@polywrap/ipfs-plugin-js";
+import { ensResolverPlugin } from "@polywrap/ens-resolver-plugin-js";
+import { ensAddress } from "@polywrap/test-env-js";
 
 
 
 (async () => {
+
+    export const ensAddress = {
+      ensAddress: "0xe78A0F7E598Cc8b0Bb87894B0F60dD2a88d6a8Ab",
+      resolverAddress: "0x5b1869D9A4C187F2EAa108f3062412ecf0526b24",
+      registrarAddress: "0xD833215cBcc3f914bD1C9ece3EE7BF8B14f841bb",
+      reverseAddress: "0xe982E462b094850F12AF94d21D470e21bE9D0E9C",
+    } as const;
 
     //let client = new PolywrapClient();
     let client = new PolywrapClient({
@@ -14,9 +23,27 @@ import { ipfsPlugin } from "@polywrap/ipfs-plugin-js";
               plugin: ipfsPlugin({
                 provider: "http://localhost:8000",
               })
+        },
+        {
+          uri: "wrap://ens/ens-resolver.polywrap.eth",
+          plugin: ensResolverPlugin({
+            addresses: {
+              testnet: ensAddress,
+            },
+          }),
         }
       ]
     });
+    /*
+    let client = new PolywrapClient({
+          uri: "wrap://ens/ens-resolver.polywrap.eth",
+          plugin: ensResolverPlugin({
+            addresses: {
+              testnet: ensAddress,
+            },
+          }),
+        });
+    */
 
     let uri = new Uri("wrap://ipfs/QmYYKkBcxVHCBbuCtMZ57ZE2NsKzZyLVskv8cX4nSzB27P").uri;
     //let uri = "ipfs/QmYYKkBcxVHCBbuCtMZ57ZE2NsKzZyLVskv8cX4nSzB27P";
