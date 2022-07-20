49928a59c2655a87b0003b23eca87114676a2719
Jovansonlee Cesar <ivanceras@gmail.com>
2022-07-20 15:04:19 +0800

    feat: make use of ethereum-plugin and ens-resolver plugin to automatically use the deployed ipfs hash in the same ENS domain

--- protocol/substrate/examples/forum-app/index.ts
+++ protocol/substrate/examples/forum-app/index.ts
@@ -1,26 +1,34 @@
 import { PolywrapClient } from "@polywrap/client-js";
 import { ipfsPlugin } from "@polywrap/ipfs-plugin-js";
 import { ethereumPlugin } from "@polywrap/ethereum-plugin-js";
+import { ensResolverPlugin } from "@polywrap/ens-resolver-plugin-js";
 
-let module_uri = "//ens/testnet/polywrap.substrate.eth";
+let module_uri = "wrap://ens/testnet/substrate.polywrap.eth";
 
 class PolywrapClientWrapper extends PolywrapClient{
     constructor() {
         super({
-          plugins: [
-            {
-                  uri: "wrap://ens/ethereum.eth",
-                  plugin: ethereumPlugin({
-                    networks: {
-                      testnet: {
-                        provider: "http://localhost:8545",
-                      },
+            plugins: [
+              {
+                uri: "wrap://ens/ens-resolver.polywrap.eth",
+                plugin: ensResolverPlugin({
+                  addresses: {
+                    testnet: "0xe78A0F7E598Cc8b0Bb87894B0F60dD2a88d6a8Ab"
+                  }
+                }),
+              },
+              {
+                uri: "wrap://ens/ethereum.polywrap.eth",
+                plugin: ethereumPlugin({
+                  networks: {
+                    testnet: {
+                      provider: "http://localhost:8545",
                     },
-                    defaultNetwork: "testnet",
-                  }),
-             },
-          ]
-        })
+                  }
+                }),
+              },
+            ]
+          })
     }
 
     async invoke_method(method, args){
