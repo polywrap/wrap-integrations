ae24d2410e5ad0ec9215b952b4a1eb1c97e120cc
krisbitney <kristoferbitney@hotmail.com>
2022-07-26 13:03:28 +0300

    rewrote swap method

--- protocol/ethereum/uniswapv2/wrapper/src/entities/swap.ts
+++ protocol/ethereum/uniswapv2/wrapper/src/entities/swap.ts
@@ -8,6 +8,7 @@ import {
   Ethereum_TxResponse,
   getChainIdKey,
   Pair,
+  Route,
   SwapParameters,
   Trade,
   TradeType,
@@ -16,7 +17,8 @@ import {
 import { getSwapMethodAbi, UNISWAP_ROUTER_CONTRACT } from "../utils";
 import { swapCallParameters, toHex } from "./router";
 import { fetchPairData } from "./fetch";
-import { bestTradeExactIn, bestTradeExactOut } from "./trade";
+import { createTrade } from "./trade";
+import { createRoute } from "./route";
 
 import { BigInt } from "@polywrap/wasm-as";
 
@@ -60,34 +62,24 @@ export function execCall(args: Args_execCall): Ethereum_TxResponse {
 }
 
 export function swap(args: Args_swap): Ethereum_TxResponse {
-  let trade: Trade;
   const pair: Pair = fetchPairData({
     token0: args.tokenIn,
     token1: args.tokenOut,
   });
-
-  if (args.tradeType == TradeType.EXACT_INPUT) {
-    trade = bestTradeExactIn({
-      pairs: [pair],
-      amountIn: {
-        token: args.tokenIn,
-        amount: args.amount,
-      },
-      tokenOut: args.tokenOut,
-      options: null,
-    })[0];
-  } else {
-    trade = bestTradeExactOut({
-      pairs: [pair],
-      amountOut: {
-        token: args.tokenOut,
-        amount: args.amount,
-      },
-      tokenIn: args.tokenIn,
-      options: null,
-    })[0];
-  }
-
+  const route: Route = createRoute({
+    pairs: [pair],
+    input: args.tokenIn,
+    output: args.tokenOut,
+  });
+  const trade: Trade = createTrade({
+    route,
+    amount: {
+      token:
+        args.tradeType == TradeType.EXACT_INPUT ? args.tokenIn : args.tokenOut,
+      amount: args.amount,
+    },
+    tradeType: args.tradeType,
+  });
   return exec({
     trade: trade,
     tradeOptions: args.tradeOptions,
