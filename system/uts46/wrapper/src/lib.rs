bd9792505c33f723e82cef110a6f1b3e03d86917
krisbitney <kristoferbitney@hotmail.com>
2022-08-12 22:26:49 +0400

    added uts46 wrapper with tests passing

--- system/uts46/wrapper/src/lib.rs
+++ system/uts46/wrapper/src/lib.rs
@@ -1,18 +1,72 @@
 pub mod wrap;
 pub use wrap::*;
+use unic_idna;
+use regex::Regex;
 
 pub fn to_ascii(args: ArgsToAscii) -> String {
-    return String::from("");
-}
-
-pub fn to_unicode(args: ArgsToUnicode) -> String {
-    return String::from("");
-}
-
-pub fn convert(args: ArgsConvert) -> ConvertResult {
-    return ConvertResult {
-        i_d_n: String::from(""),
-        p_c: String::from(""),
+    let flags = match args.flags {
+        None => unic_idna::Flags {
+            use_std3_ascii_rules: false,
+            transitional_processing: false,
+            verify_dns_length: false
+        },
+        Some(flags) => unic_idna::Flags {
+            use_std3_ascii_rules: flags.use_std3_ascii_rules.unwrap_or_default(),
+            transitional_processing: flags.transitional_processing.unwrap_or_default(),
+            verify_dns_length: flags.verify_dns_length.unwrap_or_default(),
+        },
+    };
+    match unic_idna::to_ascii(&args.value, flags) {
+        Ok(ascii) => ascii,
+        Err(e) => panic!("{:?}", e)
     }
 }
 
+pub fn to_unicode(args: ArgsToUnicode) -> String {
+    let flags = match args.flags {
+        None => unic_idna::Flags {
+            use_std3_ascii_rules: false,
+            transitional_processing: false,
+            verify_dns_length: false
+        },
+        Some(flags) => unic_idna::Flags {
+            use_std3_ascii_rules: flags.use_std3_ascii_rules.unwrap_or_default(),
+            transitional_processing: flags.transitional_processing.unwrap_or_default(),
+            verify_dns_length: flags.verify_dns_length.unwrap_or_default(),
+        },
+    };
+    match unic_idna::to_unicode(&args.value, flags) {
+        (unicode, Ok(_)) => unicode,
+        (unicode, Err(e)) => unicode // panic!("{:?}", e), TODO: change return type to include errors?
+    }
+}
+
+pub fn convert(args: ArgsConvert) -> ConvertResult {
+    // determine whether transitional processing is necessary
+    let re = Regex::new(r"\.(?:be|ca|de|fr|pm|re|swiss|tf|wf|yt)\.?$").unwrap();
+    let pc_flags = unic_idna::Flags {
+        use_std3_ascii_rules: false,
+        transitional_processing: re.is_match(&args.value),
+        verify_dns_length: false
+    };
+
+    // get pc result
+    let p_c_result = unic_idna::to_ascii(&args.value, pc_flags);
+    if p_c_result.is_err() {
+        return ConvertResult {
+            i_d_n: args.value.clone(),
+            p_c: args.value
+        };
+    }
+    let p_c: String = p_c_result.unwrap();
+
+    // get idn result
+    let idn_flags = unic_idna::Flags {
+        use_std3_ascii_rules: false,
+        transitional_processing: false,
+        verify_dns_length: false
+    };
+    let (i_d_n, _) = unic_idna::to_unicode(&args.value, idn_flags);
+
+    ConvertResult { i_d_n, p_c }
+}
