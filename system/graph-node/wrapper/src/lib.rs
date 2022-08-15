5f8c9c85f93de555c34c3da217075f76f5cb0aab
krisbitney <kristoferbitney@hotmail.com>
2022-08-15 17:34:43 +0400

    replaced value passes with ref passes where possible in graph-node wrapper

--- system/graph-node/wrapper/src/lib.rs
+++ system/graph-node/wrapper/src/lib.rs
@@ -26,17 +26,17 @@ pub fn query_subgraph(args: ArgsQuerySubgraph, env: Env) -> String {
     let ArgsQuerySubgraph { subgraph_author, subgraph_name, query } = args;
 
     // prepare http request
-    let url: String = format!("{}/subgraphs/name/{}/{}", env.provider, subgraph_author, subgraph_name);
+    let url: String = format!("{}/subgraphs/name/{}/{}", &env.provider, &subgraph_author, &subgraph_name);
     let request: Option<HttpRequest> = Some(HttpRequest {
         headers: None,
         url_params: None,
         response_type: HttpResponseType::TEXT,
-        body: Some(JSON::json!({ "query": query }).to_string()),
+        body: Some(JSON::json!({ "query": &query }).to_string()),
     });
 
     // send http request and get response body
     let data: String = match HttpModule::post(&ArgsPost { url, request }) {
-        Err(e) => panic!("GraphNode Wrapper: errors encountered. Error: {}", e),
+        Err(e) => panic!("GraphNode Wrapper: errors encountered. Error: {}", &e),
         Ok(None) => panic!("GraphNode Wrapper: response is undefined."),
         Ok(Some(response)) => match response.body {
             None => panic!("GraphNode Wrapper: response body is undefined."),
@@ -48,15 +48,15 @@ pub fn query_subgraph(args: ArgsQuerySubgraph, env: Env) -> String {
     if let Ok(request_error) = JSON::from_str::<RequestError>(&data) {
         let e: String = request_error.errors.iter().map(|e| -> String {
             if e.locations.is_none() {
-                return format!("\n-Message: {}", e.message);
+                return format!("\n-Message: {}", &e.message);
             }
             let locations: String = e.locations.as_ref().unwrap().iter()
                 .map(|loc| format!("(col: {}, line: {})", loc.column, loc.line))
                 .collect::<Vec<String>>()
                 .join(", ");
-            return format!("\n -Locations: {} \n-Message: {}", locations, e.message);
+            return format!("\n -Locations: {} \n-Message: {}", &locations, &e.message);
         }).collect::<Vec<String>>().join("\n");
-        panic!("GraphNodePlugin: errors in query string. Errors: {}", e);
+        panic!("GraphNodePlugin: errors in query string. Errors: {}", &e);
     }
 
     return data;
