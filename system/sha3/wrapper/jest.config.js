0d7e0e2a9e11054cfde18fdc9e7547a3f9ecacfe
krisbitney <kristoferbitney@hotmail.com>
2022-08-15 16:51:35 +0400

    replaced value passes with ref passes where possible in sha3 wrapper

--- system/sha3/wrapper/jest.config.js
+++ system/sha3/wrapper/jest.config.js
@@ -2,7 +2,7 @@ module.exports = {
   collectCoverage: false,
   preset: "ts-jest",
   testEnvironment: "node",
-  testMatch: ["**/__tests__/e2e/**/?(*.)+(spec|test).[jt]s?(x)"],
+  testMatch: ["**/__tests__/**/?(*.)+(spec|test).[jt]s?(x)"],
   globals: {
     "ts-jest": {
       tsconfig: "tsconfig.json",
