59a9c76a250026f9f7ccebd1e678f0711fc4832d
krisbitney <kristoferbitney@hotmail.com>
2022-08-19 20:47:41 +0400

    removed plugin config from ipfsResolverPlugin instance in ens wrapper test config

--- protocol/ethereum/ens/wrapper/src/__tests__/utils.ts
+++ protocol/ethereum/ens/wrapper/src/__tests__/utils.ts
@@ -25,7 +25,7 @@ export function getPlugins(
     },
     {
       uri: "ens/ipfs-resolver.polywrap.eth",
-      plugin: ipfsResolverPlugin({ provider: ipfs, fallbackProviders: ["ipfs.wrappers.io"] }),
+      plugin: ipfsResolverPlugin({}),
     },
     {
       uri: "ens/ipfs.polywrap.eth",
