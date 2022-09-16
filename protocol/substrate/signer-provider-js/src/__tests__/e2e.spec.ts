96156c4a02dff45fa30d65f90395f98dfcd8b78d
Willem Olding <willemolding@gmail.com>
2022-09-16 18:00:44 -0400

    adds smoke test showing mock extension can actually sign something

--- protocol/substrate/signer-provider-js/src/__tests__/e2e.spec.ts
+++ protocol/substrate/signer-provider-js/src/__tests__/e2e.spec.ts
@@ -2,6 +2,8 @@ import { PolywrapClient } from "@polywrap/client-js";
 import { substrateSignerProviderPlugin } from "../";
 import { enableFn } from "./mockExtensionInjector";
 import { injectExtension } from '@polkadot/extension-inject';
+import { web3Accounts, web3Enable, web3FromSource } from '@polkadot/extension-dapp';
+import { stringToHex } from "@polkadot/util";
 
 import { Account } from '../wrap';
 
@@ -40,5 +42,24 @@ describe("e2e", () => {
     expect(accounts[0].meta.name).toBe("alice");
   });
 
-  
+  it("Can use injected provider to sign data (smoke test)", async () => {
+    await web3Enable('e2e testing dApp');
+    const allAccounts = await web3Accounts();
+    const account = allAccounts[0];
+    const injector = await web3FromSource(account.meta.source);
+    const signRaw = injector?.signer?.signRaw;
+
+    if (!!signRaw) {
+        // WHY DO I NEED TO BIND HERE??
+        const { signature } = await signRaw.bind(injector.signer)({
+            address: account.address,
+            data: stringToHex('message to sign'),
+            type: 'bytes'
+        });
+        console.log(signature);
+    }
+
+  });
+
+
 });
