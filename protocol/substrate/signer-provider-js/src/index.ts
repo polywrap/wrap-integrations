b60ec5a425b28087156a4722aa9679e42e9178d9
Willem Olding <willemolding@gmail.com>
2022-09-19 11:51:21 -0400

    add wrappers for provider signing methods

--- protocol/substrate/signer-provider-js/src/index.ts
+++ protocol/substrate/signer-provider-js/src/index.ts
@@ -1,6 +1,7 @@
 import {
   Args_getAccounts,
-  Args_signAndSubmitExtrinsic,
+  Args_signPayload,
+  Args_signRaw, 
   Module,
   manifest,
   SignerResult,
@@ -8,7 +9,8 @@ import {
 } from "./wrap";
 
 import { Client, PluginFactory } from "@polywrap/core-js";
-import { web3Accounts, web3Enable } from '@polkadot/extension-dapp';
+import { web3Accounts, web3Enable, web3FromSource } from '@polkadot/extension-dapp';
+import type { Signer } from '@polkadot/api/types';
 
 export interface SubstrateSignerProviderPluginConfig {}
 
@@ -27,15 +29,30 @@ export class SubstrateSignerProviderPlugin extends Module<SubstrateSignerProvide
     return await web3Accounts();
   }
 
-  async signAndSubmitExtrinsic(
-    args: Args_signAndSubmitExtrinsic,
+  async signPayload(
+    { payload }: Args_signPayload,
     client: Client
   ): Promise<SignerResult> {
     await this._enableProvider();
-    return {
-      id: 0,
-      signature: "foo"
+    const { address } = payload;
+    const signer = await this._getSigner(address);
+    if (!signer || !signer?.signPayload) {
+      throw new Error("Provider for account: " + address + " does not have payload signing capabilities");
     }
+    return signer.signPayload(payload);
+  }
+
+  async signRaw(
+    { payload }: Args_signRaw,
+    client: Client
+  ): Promise<SignerResult> {
+    await this._enableProvider();
+    const { address } = payload;
+    const signer = await this._getSigner(address);
+    if (!signer || !signer?.signRaw) {
+      throw new Error("Provider for account: " + address + " does not have raw signing capabilities");
+    }
+    return signer.signRaw({ ...payload, type: 'bytes' });
   }
 
   private async _enableProvider(): Promise<void> {
@@ -44,6 +61,25 @@ export class SubstrateSignerProviderPlugin extends Module<SubstrateSignerProvide
     }
     await web3Enable("substrate-signer-provider-plugin");
     this._isProviderEnabled = true;
+  }
+
+
+  /**
+   * Searches the injected web3 providers for any accounts that match the 
+   * given address and then return associated Signer.
+   *
+   * Requires this._enableProvider() be called first
+   */
+  private async _getSigner(address: String): Promise<Signer> {
+    const accounts = await await web3Accounts();
+    const signingAccount = accounts.find(acc => acc.address == address);
+
+    if (!signingAccount) {
+      throw new Error("Provider does not contain account: " + address);
+    }
+
+    const injector = await web3FromSource(signingAccount.meta.source);
+    return injector?.signer
   }
 }
 
