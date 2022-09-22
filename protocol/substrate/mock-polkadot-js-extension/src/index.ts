90da48e8b32673012f045af58df9ce1d78a6b364
Willem Olding <willemolding@gmail.com>
2022-09-22 14:37:47 -0400

    add new package for extension injector

---
+++ protocol/substrate/mock-polkadot-js-extension/src/index.ts
@@ -0,0 +1,29 @@
+import { Injected, InjectedAccount } from '@polkadot/extension-inject/types';
+
+import { Keyring } from '@polkadot/keyring';
+import { cryptoWaitReady } from '@polkadot/util-crypto';
+import { SingleAccountSigner } from './singleAccountSigner';
+import { TypeRegistry } from '@polkadot/types';
+
+// create a mock polkadot-js extension and inject into the environment
+export async function enableFn (originName: string): Promise<Injected> {
+
+  await cryptoWaitReady();
+
+  // create a keyring
+  const keyring = new Keyring({ type: 'sr25519' });
+  const alice = keyring.createFromUri('//Alice');
+  const registry = new TypeRegistry();
+  const signer = new SingleAccountSigner(registry, alice);
+
+  // These accounts must be valid sr25519 addresses or they will get filtered out by the web3Accounts function
+  const accounts: InjectedAccount[] = [{ address: alice.address, name: "alice", type: 'sr25519' }];
+
+  return {
+    accounts: {
+      get: async () => accounts,
+      subscribe: (cb) => { cb(accounts); return () => {} }
+    },
+    signer
+  }
+}
