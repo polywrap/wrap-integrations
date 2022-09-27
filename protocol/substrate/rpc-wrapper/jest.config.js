4d9333ea99156572919caf8d3cc18ad33944439e
Willem Olding <willemolding@gmail.com>
2022-09-27 13:57:31 -0400

    break again by changing to jsdom

--- protocol/substrate/rpc-wrapper/jest.config.js
+++ protocol/substrate/rpc-wrapper/jest.config.js
@@ -1,7 +1,7 @@
 module.exports = {
   collectCoverage: true,
   preset: 'ts-jest',
-  testEnvironment: 'node',
+  testEnvironment: 'jsdom',
   testMatch: ["**/tests/*.spec.ts"],
   globals: {
     'ts-jest': {
