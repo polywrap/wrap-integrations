9e4b0e036b2dca6a7d7703bc14e3d65b04cbe39e
krisbitney <kristoferbitney@hotmail.com>
2022-10-10 12:38:43 +0500

    fixed providers length check

--- protocol/ipfs/wrapper-ipfs-exec/src/lib.rs
+++ protocol/ipfs/wrapper-ipfs-exec/src/lib.rs
@@ -5,7 +5,7 @@ pub use util::*;
 
 pub fn cat(args: ArgsCat, env: Env) -> Vec<u8> {
     let options: Options = get_options(&args.options, &env);
-    if options.disable_parallel_requests || options.providers.len() == 0 {
+    if options.disable_parallel_requests || options.providers.len() == 1 {
         let exec: &dyn Fn(&str) -> Result<Vec<u8>, String> =
             &|provider: &str| exec_cat(provider, args.cid.as_ref(), options.timeout);
         return exec_sequential(options.providers, exec);
@@ -17,7 +17,7 @@ pub fn cat(args: ArgsCat, env: Env) -> Vec<u8> {
 
 pub fn resolve(args: ArgsResolve, env: Env) -> Option<IpfsResolveResult> {
     let options: Options = get_options(&args.options, &env);
-    if options.disable_parallel_requests || options.providers.len() == 0 {
+    if options.disable_parallel_requests || options.providers.len() == 1 {
         let exec: &dyn Fn(&str) -> Result<IpfsResolveResult, String> =
             &|provider: &str| exec_resolve(provider, args.cid.as_ref(), options.timeout);
         return Some(exec_sequential(options.providers, exec));
@@ -29,7 +29,7 @@ pub fn resolve(args: ArgsResolve, env: Env) -> Option<IpfsResolveResult> {
 
 pub fn add_file(args: ArgsAddFile, env: Env) -> String {
     let options: Options = get_options(&None, &env);
-    if options.disable_parallel_requests || options.providers.len() == 0 {
+    if options.disable_parallel_requests || options.providers.len() == 1 {
         let exec: &dyn Fn(&str) -> Result<String, String> =
             &|provider: &str| exec_add_file(provider, &args.data, options.timeout);
         return exec_sequential(options.providers, exec);
