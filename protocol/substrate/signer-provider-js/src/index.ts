08d60c43ebc46b837a4186786daf66a209c662ba
dOrgJelli <jelli@dorg.tech>
2022-12-05 22:00:37 -0500

    feat: support provider config option

--- protocol/substrate/signer-provider-js/src/index.ts
+++ protocol/substrate/signer-provider-js/src/index.ts
@@ -8,23 +8,25 @@ import {
   Account,
   SignerPayloadJSON
 } from "./wrap";
-import type { SignerPayloadJSON as StringifiedSignerPayloadJSON } from '@polkadot/types/types';
+import { SignerProvider } from "./SignerProvider";
+import { InjectedSignerProvider } from "./providers/InjectedSignerProvider";
 
+import { SignerPayloadJSON as StringifiedSignerPayloadJSON } from '@polkadot/types/types';
 import { Client, PluginFactory } from "@polywrap/core-js";
-import { web3Accounts, web3Enable, web3FromSource } from '@polkadot/extension-dapp';
-import type { Signer } from '@polkadot/api/types';
-import { TypeRegistry } from '@polkadot/types';
 import { UInt } from '@polkadot/types-codec';
 
-export interface SubstrateSignerProviderPluginConfig {}
+export interface SubstrateSignerProviderPluginConfig {
+  provider?: SignerProvider;
+}
 
 export class SubstrateSignerProviderPlugin extends Module<SubstrateSignerProviderPluginConfig> {
-  private _isProviderEnabled: boolean = false;
-  private _registry: TypeRegistry;
+  private _provider: SignerProvider;
 
   constructor(config: SubstrateSignerProviderPluginConfig) {
     super(config);
-    this._registry = new TypeRegistry();
+
+    // If no provider is provided, use the injected signer/provider (polkadot.js extension)
+    this._provider = config.provider || new InjectedSignerProvider();
   }
 
   /**
@@ -35,8 +37,7 @@ export class SubstrateSignerProviderPlugin extends Module<SubstrateSignerProvide
     args: Args_getAccounts,
     client: Client
   ): Promise<Account[]> {
-    await this._enableProvider();
-    return await web3Accounts();
+    return await this._provider.getAccounts();
   }
 
   /**
@@ -45,17 +46,16 @@ export class SubstrateSignerProviderPlugin extends Module<SubstrateSignerProvide
    * displays a more informative dialog to the user
    */
   async signPayload(
-    { payload }: Args_signPayload,
+    args: Args_signPayload,
     client: Client
   ): Promise<SignerResult> {
-    await this._enableProvider();
-    const { address } = payload;
-    const signer = await this._getSigner(address);
+    const address = args.payload.address;
+    const signer = await this._provider.getSigner(address);
     if (!signer || !signer?.signPayload) {
       throw new Error("Provider for account: " + address + " does not have payload signing capabilities");
     }
     return signer.signPayload(
-      this._stringifyPayloadFields(payload)
+      this._stringifyPayloadFields(args.payload)
     );
   }
 
@@ -65,43 +65,15 @@ export class SubstrateSignerProviderPlugin extends Module<SubstrateSignerProvide
    * This is opaque so the user may not know what they are signing.
    */
   async signRaw(
-    { payload }: Args_signRaw,
+    args: Args_signRaw,
     client: Client
   ): Promise<SignerResult> {
-    await this._enableProvider();
-    const { address } = payload;
-    const signer = await this._getSigner(address);
+    const address = args.payload.address;
+    const signer = await this._provider.getSigner(address);
     if (!signer || !signer?.signRaw) {
       throw new Error("Provider for account: " + address + " does not have raw signing capabilities");
     }
-    return signer.signRaw({ ...payload, type: 'bytes' });
-  }
-
-  private async _enableProvider(): Promise<void> {
-    if (this._isProviderEnabled) {
-      return;
-    }
-    await web3Enable("substrate-signer-provider-plugin");
-    this._isProviderEnabled = true;
-  }
-
-
-  /**
-   * Searches the injected web3 providers for any accounts that match the 
-   * given address and then return associated Signer.
-   *
-   * Requires this._enableProvider() be called first
-   */
-  private async _getSigner(address: String): Promise<Signer> {
-    const accounts = await await web3Accounts();
-    const signingAccount = accounts.find(acc => acc.address == address);
-
-    if (!signingAccount) {
-      throw new Error("Provider does not contain account: " + address);
-    }
-
-    const injector = await web3FromSource(signingAccount.meta.source);
-    return injector?.signer
+    return signer.signRaw({ ...args.payload, type: 'bytes' });
   }
 
   /**
@@ -109,13 +81,14 @@ export class SubstrateSignerProviderPlugin extends Module<SubstrateSignerProvide
    * This performs that conversion from the numeric types provided by the GraphQL generated type of the payload
    */
   private _stringifyPayloadFields(payload: SignerPayloadJSON): StringifiedSignerPayloadJSON {
+    const registry = this._provider.registry;
     return {
       ...payload,
-      nonce: new UInt(this._registry, payload.nonce, 32).toHex(),
-      specVersion: new UInt(this._registry, payload.specVersion, 32).toHex(),
-      tip: new UInt(this._registry, payload.tip, 128).toHex(),
-      transactionVersion: new UInt(this._registry, payload.transactionVersion, 32).toHex(),
-      blockNumber: new UInt(this._registry, payload.blockNumber, 32).toHex(),
+      nonce: new UInt(registry, payload.nonce, 32).toHex(),
+      specVersion: new UInt(registry, payload.specVersion, 32).toHex(),
+      tip: new UInt(registry, payload.tip, 128).toHex(),
+      transactionVersion: new UInt(registry, payload.transactionVersion, 32).toHex(),
+      blockNumber: new UInt(registry, payload.blockNumber, 32).toHex(),
     };
   }
 
---
+++ protocol/substrate/signer-provider-js/src/providers/KeyringSignerProvider.ts
@@ -0,0 +1,78 @@
+import { SignerProvider } from "../SignerProvider";
+import { Account } from "../wrap";
+
+import { KeyringStruct } from "@polkadot/ui-keyring/types";
+import { Signer } from '@polkadot/api/types';
+import { TypeRegistry } from '@polkadot/types';
+import { SignerPayloadJSON, SignerPayloadRaw, SignerResult } from "@polkadot/types/types";
+import { objectSpread, hexToU8a, u8aToHex } from '@polkadot/util';
+
+export class KeyringSignerProvider implements SignerProvider {
+  private _registry: TypeRegistry;
+  private _signers: Map<string, Signer>;
+
+  constructor(
+    public keyring: KeyringStruct,
+    registry?: TypeRegistry
+  ) {
+    this._registry = registry || new TypeRegistry();
+    this._signers = new Map();
+  }
+
+  public get registry(): TypeRegistry {
+    return this._registry;
+  }
+
+  public async getAccounts(): Promise<Account[]> {
+    return this.keyring.getAccounts();
+  }
+
+  public async getSigner(address: string): Promise<Signer> {
+    let signer = this._signers.get(address);
+
+    if (signer) {
+      return signer;
+    }
+
+    const pair = this.keyring.getPair(address);
+    let id = 0;
+
+    // Implementation based on @polkadot/api/src/test/SingleAccountSigner.ts
+    signer = {
+      signPayload: async (payload: SignerPayloadJSON): Promise<SignerResult> => {
+        if (payload.address !== pair.address) {
+          throw new Error("Payload address does not match the keyringPair address.");
+        }
+
+        return new Promise((resolve): void => {
+          const signed = this.registry.createType(
+            'ExtrinsicPayload',
+            payload,
+            { version: payload.version }
+          ).sign(pair);
+  
+          resolve(objectSpread({ id: ++id }, signed));
+        });
+      },
+      signRaw: async (payload: SignerPayloadRaw): Promise<SignerResult> => {
+        if (payload.address !== pair.address) {
+          throw new Error("Payload address does not match the keyringPair address.");
+        }
+
+        return new Promise((resolve): void => {
+          const signature = u8aToHex(
+            pair.sign(hexToU8a(payload.data)),
+          );
+  
+          resolve({
+            id: ++id,
+            signature
+          });
+        });
+      }
+    };
+
+    this._signers.set(address, signer);
+    return signer;
+  }
+} 
